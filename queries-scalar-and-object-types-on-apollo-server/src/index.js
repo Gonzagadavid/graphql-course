3d88a852820a50c42b9484bdc10c9dc87012f498
gonzagadavid <david.blesses@hotmail.com>
2022-10-01 14:23:37 -0300

    feat: add Scalar types

--- queries-scalar-and-object-types-on-apollo-server/src/index.js
+++ queries-scalar-and-object-types-on-apollo-server/src/index.js
@@ -2,15 +2,23 @@ import { ApolloServer, gql } from 'apollo-server';
 
 const typeDefs = gql`
 type Query {
-  hello: String
-  hi: String
+  id: ID!
+  name: String!
+  age: Int!
+  average: Float!
+  married: Boolean!
+  arrayString: [String!]!
 } 
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'hello word!!',
-    hi: () => 'Hi!',
+    id: () => '3232esddf-2',
+    name: () => 'David',
+    age: () => 34,
+    average: () => 90.55,
+    married: () => false,
+    arrayString: () => [],
   },
 };
 const server = new ApolloServer({ typeDefs, resolvers });
