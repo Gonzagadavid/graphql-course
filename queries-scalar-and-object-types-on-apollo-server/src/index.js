a401eccba2c98ce04c1a49734ea0353ca3c448d8
gonzagadavid <david.blesses@hotmail.com>
2022-10-01 14:02:42 -0300

    feat: add resolvers intro

--- queries-scalar-and-object-types-on-apollo-server/src/index.js
+++ queries-scalar-and-object-types-on-apollo-server/src/index.js
@@ -3,9 +3,17 @@ import { ApolloServer, gql } from 'apollo-server';
 const typeDefs = gql`
 type Query {
   hello: String
+  hi: String
 } 
 `;
-const server = new ApolloServer({ typeDefs });
+
+const resolvers = {
+  Query: {
+    hello: () => 'hello word!!',
+    hi: () => 'Hi!',
+  },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
 
 // eslint-disable-next-line no-console
 server.listen(4003).then(({ url }) => { console.log(`Server listening in ${url}`); });
