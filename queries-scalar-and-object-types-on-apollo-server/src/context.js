3b6aef8ac0a9ec561f9b1ca95a884ad86552f84d
gonzagadavid <david.blesses@hotmail.com>
2022-10-02 01:01:06 -0300

    feat: add getPosts in context

--- queries-scalar-and-object-types-on-apollo-server/src/context.js
+++ queries-scalar-and-object-types-on-apollo-server/src/context.js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 
 export const context = () => ({
-  getUsers: (path = '') => fetch((`http://localhost:3000/users/${path}`)),
+  getUsers: (path = '') => fetch(`http://localhost:3000/users/${path}`),
+  getPosts: (path = '') => fetch(`http://localhost:3000/posts/${path}`),
 });
